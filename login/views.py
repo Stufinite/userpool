c1b35c8a536a103ee710f4f2518fe0b5f664256c
CJHwong <cjhwong@gmail.com>
2016-10-04 12:06:56 +0800

    Fixed error when user typed wrong profile data

--- login/views.py
+++ login/views.py
@@ -80,10 +80,10 @@ def forgot_password(request):
                 return render(request, 'success.html', {'title': '密碼已寄送', 'context': '前往信箱取得新的密碼'})
         except User.DoesNotExist:
             form = UserForgotPasswordForm()
-            return render(request, 'forgot.html', {'form': form, 'type_error': True})
+        return render(request, 'forgot.html', {'form': form, 'type_error': True})
     else:
         form = UserForgotPasswordForm()
-    return render(request, 'forgot.html', {'form': form})
+        return render(request, 'forgot.html', {'form': form})
 
 
 @login_required
