95a0a868559e51a197b8f3c53a54347901e24ac5
CJHwong <cjhwong@gmail.com>
2016-10-11 13:45:20 +0800

    Added middleware which use cache to store session

--- userpool/settings.py
+++ userpool/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+
 ]
 
 ROOT_URLCONF = 'userpool.urls'
